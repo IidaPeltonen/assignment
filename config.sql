fdedf247ac100db5d87250f992696b27beb7d537
IidaPeltonen <n0peii00@students.oamk.fi>
2021-12-03 19:50:56 +0200

    ok

--- config.sql
+++ config.sql
@@ -3,6 +3,6 @@ drop database if exists n0peii00;
 -- luodaan uusi
 CREATE database n0peii00;
 --luodaan taulu tunnus
-CREATE table tunnus (id int primary key AUTO_INCREMENT, user varCHAR(50) NOT null, password varchar(150) not null);
+CREATE table tunnus (user varCHAR(50) primary key NOT null, password varchar(150) not null);
 --luodaan taulu tiedot
-CREATE table tiedot (id int not null, etunimi char(50), sukunimi char(50), email char(100), foreign key (id) references tunnus(id));
+CREATE table tiedot (user varCHAR(50), etunimi char(50), sukunimi char(50), email char(100), foreign key (user) references tunnus(user));
